6c93758096e5642410ece88d5ff0500822dcba29
straga44 <danil.gunn@yandex.ru>
2025-11-15 17:56:39 +0400

    added talents and abilities insted of generic spells

--- src/main.js
+++ src/main.js
@@ -11,14 +11,17 @@ async function loadData(path) {
 async function initGame() {
   const enemiesData = await loadData("./src/data/enemies.json");
   const itemsData   = await loadData("./src/items/items.json");
-  const spellsData  = await loadData("./src/spells/spells.json");
+  const abilitiesData  = await loadData("./src/abilities/abilities.json");
   const levelsData  = await loadData("./src/levels/levels.json");
+  const talentsData  = await loadData("./src/talents/talents.json");
+
 
   console.log("Player:", player);
   console.log("Enemies:", enemiesData);
   console.log("Items:", itemsData);
-  console.log("Spells:", spellsData);
+  console.log("Abilities:", abilitiesData);
   console.log("Levels:", levelsData);
+
 }
 
 initGame();
